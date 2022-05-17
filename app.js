0f14671466007dca1e72d0094c9bdba3a62848b5
usa3amustafa <usa3a3ustafa@outlook.com>
2022-05-17 21:22:27 +0500

    changes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const openMenuBtn = document.querySelector('.menu-icon-top')
 const closeMenuBtn = document.querySelector('.menu-icon-bottom')
 const links = document.querySelector('.links')
+const link = document.querySelector('.link')
 
 openMenuBtn.addEventListener('click', function () {
   links.classList.add('show-links')
   closeMenuBtn.classList.add('show-btn')
-  openMenuBtn.classList.remove('show-btn')
+  openMenuBtn.style.display = 'none'
+  link.classList.add('padding-top')
 })
 closeMenuBtn.addEventListener('click', function () {
   links.classList.remove('show-links')
   closeMenuBtn.classList.remove('show-btn')
-  openMenuBtn.classList.add('show-btn')
+  openMenuBtn.style.display = 'block'
+  link.classList.remove('padding-top')
 })
